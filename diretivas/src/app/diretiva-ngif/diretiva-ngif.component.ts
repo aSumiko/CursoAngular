acf8142f8acffced1ab3ce1c0bb91111fe8668e8
aSumiko <ana.sumiko@gmail.com>
2023-07-24 18:39:28 -0300

    Diretivas_ngIf

---
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.scss']
+})
+export class DiretivaNgifComponent {
+
+  cursos: string[] = ["Angular 2"];
+
+  mostrarCursos: boolean = false;
+  onMostrarCursos(){
+    this.mostrarCursos = !this.mostrarCursos;
+  }
+
+
+
+}
